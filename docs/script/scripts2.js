b8166a2161a7601da9438148fac9666efc43b509
Marten Moolenaar <marten@zodan.nl>
2024-02-07 11:35:32 +0100

    All first ext

--- docs/script/scripts2.js
+++ docs/script/scripts2.js
@@ -1,24 +1,24 @@
 main().then(() => console.log('done'))
 
 async function main() {
-    const students = await getStudents();
-    renderStudents(students.sort(() => 0.5 - Math.random()))
+    const members = await getMembers();
+    renderMembers(members.members(() => 0.5 - Math.random()))
 }
 
-function renderStudents(students) {
+function renderMembers(members) {
     const container = document.querySelector('[data-students]');
     const list = document.createElement('ul')
 
-    students.map(student => {
+    members.map(member => {
         const item = document.createElement('li')
         const anchor = document.createElement('a');
         const description = document.createElement('p')
         const avatar = document.createElement('img')
-        anchor.href = `https://${student.login}.github.io/web-app-from-scratch-2324/`
-        anchor.alt = `WAFS fork from ${student.login}`
+        // anchor.href = `https://${student.login}.github.io/web-app-from-scratch-2324/`
+        anchor.alt = `WAFS fork from ${member.firstName}`
         anchor.target = '_blank'
-        anchor.textContent = student.login
-        avatar.src = student.avatar_url
+        anchor.textContent = member.firstName + ' ' + member.lastName
+        avatar.src = member.avatar_url
         item.append(avatar)
         item.append(anchor)
         list.append(item)
@@ -26,8 +26,8 @@ function renderStudents(students) {
     container.append(list)
 }
 
-async function getStudents() {
-    const res = await fetch('https://api.github.com/repos/cmda-minor-web/web-app-from-scratch-2324/forks')
+async function getMembers() {
+    const res = await fetch('./')
     const teams = await res.json()
     console.log(teams);
     return teams.map(({owner}) => owner)
@@ -39,65 +39,65 @@ async function getStudents() {
 
 
 
-const personalSiteInfoMarten = {
-    "name": "Marten",
-    "avatar" : "https://github.com/utueytbuiewbty.webp",
-    "dateOfBirth" : {
-        "year" : 1999,
-        "month" : 4,
-        "day" : 1,
+// const personalSiteInfoMarten = {
+//     "name": "Marten",
+//     "avatar" : "https://github.com/utueytbuiewbty.webp",
+//     "dateOfBirth" : {
+//         "year" : 1999,
+//         "month" : 4,
+//         "day" : 1,
 
-    },
-    "favorietPets" : [
-        "shrimp"
-    ],
-    "openForJobProposals" : false
-}
+//     },
+//     "favorietPets" : [
+//         "shrimp"
+//     ],
+//     "openForJobProposals" : false
+// }
 
-const teamMembers = {
-    "members": [
-        {
-            "name" : "Aap34",
-            "personalPage" : "https://aap34nogeenkeer.github.io/WAFS/app.json",
-            "testData" : personalSiteInfoMarten
-        },
-        {
-            "name" : "Noot"
-        },
-        {
-            "name" : "Mies"
-        },
-        {
-            "name" : "Teun"
-        }
+// const teamMembers = {
+//     "members": [
+//         {
+//             "name" : "Aap34",
+//             "personalPage" : "https://aap34nogeenkeer.github.io/WAFS/app.json",
+//             "testData" : personalSiteInfoMarten
+//         },
+//         {
+//             "name" : "Noot"
+//         },
+//         {
+//             "name" : "Mies"
+//         },
+//         {
+//             "name" : "Teun"
+//         }
 
-    ]
-}
-
-
+//     ]
+// }
 
 
 
-showMember(teamMembers.members[0]);
 
 
-function showMember(teamMember) {
-    let url = teamMember.personalPage;
-    const memberDataRequest = new Request(url);    
+// showMember(teamMembers.members[0]);
 
-    fetch(memberDataRequest)
-        .then((response) => {
-            // if (!response.ok) {
-            // throw new Error(`HTTP error! Status: ${response.status}`);
-            // }
+
+// function showMember(teamMember) {
+//     let url = teamMember.personalPage;
+//     const memberDataRequest = new Request(url); 
+
+//     fetch(memberDataRequest)
+//         .then((response) => {
+//             // if (!response.ok) {
+//             // throw new Error(`HTTP error! Status: ${response.status}`);
+//             // }
         
-         //return response.blob();
-         console.log(response);
-  })
+//          //return response.blob();
+//          console.log(response);
+//   })
 
-  // console.log(memberInfo);
-}
-showMember();
+//   // console.log(memberInfo);
+// }
+// showMember();
 
 
 
